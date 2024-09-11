975d9df7e51e2f5da8305b009a5c63238cee9d20
W0856089 <w0856089@myscc.ca>
2024-09-11 14:45:46 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Sultan-AlShamsi-Learning-Angular';
+
+  fName:string = "Sultan"
+  lName:string = "Al Shamsi"
+
+
 }
