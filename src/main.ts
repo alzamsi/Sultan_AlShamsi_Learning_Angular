658e5825b99aaed0945c4a7c8936c13e22840513
Sultan Al Shamsi <w0856089@myscc.ca>
2024-11-12 19:57:59 -0500

    added lazy load and some css

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,19 @@ import {ModifyPhoneComponent} from './app/modify-phones/modify-phones.component'
 const routes: Routes = [
   {path:'', redirectTo: '/phones', pathMatch: 'full'},
   { path: 'phones', component: PhoneListComponent },
-  { path: 'phones/:id', component: PhoneListItemComponent },
-  {path:'modify-student/:id', component: ModifyPhoneComponent},
-  {path:'modify-student',component:ModifyPhoneComponent},
-  {path: '**', component:PageNotFoundComponent}
 
+  {path: 'phones/:id',
+  loadComponent: () =>
+  import('./app/phone-list-item/phone-list-item.component').then(m => m.PhoneListItemComponent)},
+{path: 'modify-student/:id',
+    loadComponent: () =>
+      import('./app/modify-phones/modify-phones.component').then(m => m.ModifyPhoneComponent)},
+{path: 'modify-student',
+    loadComponent: () =>
+  import('./app/modify-phones/modify-phones.component').then(m => m.ModifyPhoneComponent)},
+{path: '**',
+    loadComponent: () =>
+  import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)}
 ];
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)]
