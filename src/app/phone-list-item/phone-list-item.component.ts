bdf4b403bab8d7661de8a4b24132858b7aceba5c
Sultan Al Shamsi <w0856089@myscc.ca>
2024-11-12 19:30:32 -0500

    added pipes as well as modified my data to work with pipes

--- src/app/phone-list-item/phone-list-item.component.ts
+++ src/app/phone-list-item/phone-list-item.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import {Component, Input} from "@angular/core";
 import {Phones} from "../phones";
+import {ModelPipe} from "../pipes/model.pipe";
 
 @Component({
   selector: 'app-phone-list-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModelPipe],
   templateUrl: './phone-list-item.component.html',
   styleUrls: ['./phone-list-item.component.css']
 })
--- src/app/phones.ts
+++ src/app/phones.ts
@@ -2,6 +2,7 @@ export interface Phones {
   id: number;
   name: string;
   company: string;
+  price: number;
   storage: number;
   smartPhone: boolean;
   imageUrl: string;
