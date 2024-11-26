3979fb1dc327eb494f517d5c35a8cc6978d9e7bc
Sultan Al Shamsi <w0856089@myscc.ca>
2024-11-26 16:42:49 -0500

    created a new file called directives to add a new element HoverHighlight and used it in phones list html

--- src/app/phone-list-item/phone-list-item.component.ts
+++ src/app/phone-list-item/phone-list-item.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import {Component, Input} from "@angular/core";
 import {Phones} from "../phones";
 import {ModelPipe} from "../pipes/model.pipe";
+import {HoverHighlightDirective} from "../directives/hover-highlight.directive";
 
 @Component({
   selector: 'app-phone-list-item',
   standalone: true,
-  imports: [CommonModule, ModelPipe],
+  imports: [CommonModule, ModelPipe, HoverHighlightDirective],
   templateUrl: './phone-list-item.component.html',
   styleUrls: ['./phone-list-item.component.css']
 })
